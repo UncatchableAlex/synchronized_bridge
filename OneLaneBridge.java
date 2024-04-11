015bd960b5aaf9307a6f239763414274ae14ab0f
Alex Meislich <alex.meislich@gmail.com>
2024-04-11 00:30:32 -0700

    working example

--- OneLaneBridge.java
+++ OneLaneBridge.java
@@ -1,7 +1,5 @@
 public class OneLaneBridge extends Bridge {
     private int bridgeLimit;
-    private Object inTrafficTrueDir;
-    private Object inTrafficFalseDir;
 
     /**
      * The constructor for a OneLaneBridge
@@ -10,46 +8,33 @@ public class OneLaneBridge extends Bridge {
     public OneLaneBridge(int bridgeLimit) {
         super();
         this.bridgeLimit = bridgeLimit;
-        this.inTrafficTrueDir = new Object();
-        this.inTrafficFalseDir = new Object();
-        System.out.println(inTrafficTrueDir);
-        System.out.println(inTrafficFalseDir);
-        
     }
     @Override
     /**
      * Try to let another car onto the bridge
      * @param car This is the car that is arriving on the bridge
      */
-    public void arrive(Car car) throws InterruptedException {
-        // acquire the monitor for the correct direction:
-        Object trafficQueue = car.getDirection() ? this.inTrafficTrueDir : this.inTrafficFalseDir;
-        System.out.println("waiting for " + trafficQueue);
-        synchronized (trafficQueue) {
-            // if the bridge is empty, switch the direction to accomodate the arriving car:
-            synchronized (this) {
-                if (super.bridge.isEmpty()) {
-                    super.direction = car.getDirection();
-                }
-            }
-            // make the car wait in traffic until the car is going in the correct direction and the bridge is under capacity
-            while ((car.getDirection() != super.direction) || (super.bridge.size() >= bridgeLimit)) {
-                // if the bridge is empty, switch the direction to accomodate the arriving car:
-                if (super.bridge.isEmpty()) {
-                    super.direction = car.getDirection();
-                }
-                // make the car wait in the correct traffic queue
-                System.out.println("making " + car + " wait in " + trafficQueue);
-                trafficQueue.wait();
-            }
-            car.setEntryTime(super.currentTime);
-            // add the car 
-            super.bridge.add(car);
-            // show the cars currently on the bridge to verify that we are still number the maximum cars on bridge total
-            System.out.println(this.toString());
-            // increment the time
-            super.currentTime++;
+    public synchronized void arrive(Car car) throws InterruptedException {
+        // if the bridge is empty, switch the direction to accomodate the arriving car:
+        if (super.bridge.isEmpty()) {
+            super.direction = car.getDirection();
         }
+        // make the car wait in traffic until the car is going in the correct direction and the bridge is under capacity
+        while ((car.getDirection() != super.direction) || (super.bridge.size() >= bridgeLimit)) {
+            // if the bridge is empty, switch the direction to accomodate the arriving car:
+            if (super.bridge.isEmpty()) {
+                super.direction = car.getDirection();
+            }
+            // make the car wait in the correct traffic queue
+            this.wait();
+        }
+        car.setEntryTime(super.currentTime);
+        // add the car 
+        super.bridge.add(car);
+        // show the cars currently on the bridge to verify that we are still number the maximum cars on bridge total
+        System.out.println(this.toString());
+        // increment the time
+        super.currentTime++;
     }
 
 
@@ -58,29 +43,19 @@ public class OneLaneBridge extends Bridge {
      * Let a car on the bridge exit. If the car isn't the next car on the bridge, it will wait until it is.
      * @param car The car exiting the bridge
      */
-    public void exit(Car car) throws InterruptedException {
-        Object trafficQueue = car.getDirection() ? this.inTrafficTrueDir : this.inTrafficFalseDir;
-        synchronized(trafficQueue) {
-            // wait until this car is at the front of the line and can exit:
-            while (super.bridge.get(0).getID() != car.getID()) {
-                trafficQueue.wait();
-            }
+    public synchronized void exit(Car car) throws InterruptedException {
+        // wait until this car is at the front of the line and can exit:
+        while (super.bridge.get(0).getID() != car.getID()) {
+            this.wait();
         }
-        synchronized(this) {
-            // remove the car that wants to exit
-            super.bridge.remove(0);
-            System.out.println(this.toString());
-            // if the bridge is now empty, reverse the direction and notify all for the new direction
-            if (super.bridge.isEmpty()) {
-                super.direction = !super.direction; 
-            }
-            Object waitingQueue = super.direction ? this.inTrafficTrueDir : this.inTrafficFalseDir;
-            // notify the next car in the waiting queue:
-            synchronized(waitingQueue) {
-                System.out.println("notifying next car in " + waitingQueue);
-                waitingQueue.notify();
-            }
+        // remove the car that wants to exit
+        super.bridge.remove(0);
+        System.out.println(this.toString());
+        // if the bridge is now empty, reverse the direction and notify all for the new direction
+        if (super.bridge.isEmpty()) {
+            super.direction = !super.direction; 
         }
+        this.notifyAll();
     }
 
     @Override
